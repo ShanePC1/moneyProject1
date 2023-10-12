cc13af10ca48ef094fee85456a36b34188bb538f
Shane Orend <sorend95@outlook.com>
2023-10-12 17:32:56 -0400

    dataEntry updates
    dataEntry now adds a new select option row when add more is chosen

--- controller/index.js
+++ controller/index.js
@@ -30,6 +30,8 @@ if ($("body").data("title") === "indexPage") {
 }else if ($("body").data("title") === "dataEntry") {
 	 const monthlyBills = document.querySelector('#monthlyBillForm');
 	 const pText = document.querySelector('.pTagChkAmt');
+	 const bttnMoreRow = document.querySelector('.addMoreRows');
+	 
 	checkingStartAmtUSD=(sessionStorage.getItem(checkingStartAmtUSD));
 
 		pText.innerHTML = `Alright so you have ${checkingStartAmtUSD} in your checking currnetly.`
@@ -37,10 +39,56 @@ if ($("body").data("title") === "indexPage") {
 // To collect all the bill options I think it needs to be an array of objects.
 		//to add more rows of bills I think it needs to create the row the form the drop down etc. for each click.
 
+bttnMoreRow.addEventListener('click', function(e){
+	e.preventDefault();
+				createNewForm();
+
+})
 monthlyBills.addEventListener('submit', function(e){
 	e.preventDefault();
 				window.location = "/incomeEntry";		
 })
 
 }
+
+function createNewForm(){
+	const Billrow= document.querySelector('.bills');
+	createRow(Billrow);
+
+}
+
+function createRow(appendRow,rowId, colId,colText){
+	console.log(appendRow);
+//If text is need it will go in a column
+//need to add parameter for col quanitiy
+const row =  document.createElement('div');;
+appendRow.appendChild(row);
+row.classList.add("row");
+row.setAttribute('class', rowId);
+row.appendChild(createSelect(appendRow));
+appendRow.appendChild(row);
+return row;
+}
+
+function createSelect(colId, colText){
+const selectTag= document.createElement('select');;
+//selectTag.classList.add('col');
+//selectTag.setAttribute('id', colId);
+selectTag.innerText ='colText';
+selectTag.appendChild(createOptions());
+return selectTag;
+}
+
+
+function createOptions(){
+
+	// create an array of choices function that takes user inout and adds to array
+	//add the other row elements to create row the date and the currenyc box
+const optionTag= document.createElement('option');;
+//selectTag.classList.add('col');
+optionTag.setAttribute('value', 'Amazon');
+optionTag.innerText ='Amazon';
+return optionTag;
+}
+
 
